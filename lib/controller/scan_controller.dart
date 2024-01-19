be24793236930d37f20a086372b33c3029d09ae8
Devangana26 <devanganabarua@gmail.com>
2024-01-19 08:10:02 +0530

    model and label added

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,12 +1,17 @@
+import 'dart:math';
+import 'package:image/image.dart';
 import 'package:get/get.dart';
 import 'package:camera/camera.dart';
+// import 'package:tflite/tflite.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 
 class ScanController extends GetxController {
   @override
   void onInit() {
     super.onInit();
     initCamera();
+    initTFlite();
   }
 
   @override
@@ -18,26 +23,85 @@ class ScanController extends GetxController {
   late CameraController cameraController;
   late List<CameraDescription> cameras;
 
+  late CameraImage cameraImage;
+
   var isCameraInitialised = false.obs;
+  var cameraCount = 0;
+
+  var x, y, w, h = 0.0;
+  var label = "";
 
   initCamera() async {
-    try {
-      cameras = await availableCameras();
+    if (await Permission.camera.request().isGranted) {
+      try {
+        cameras = await availableCameras();
 
-      if (cameras.isNotEmpty) {
-        cameraController = CameraController(
-          cameras[0],
-          ResolutionPreset.max,
-        );
+        if (cameras.isNotEmpty) {
+          cameraController = CameraController(
+            cameras[0],
+            ResolutionPreset.max,
+          );
 
-        await cameraController.initialize();
-        isCameraInitialised(true);
-        update();
-      } else {
-        print("No cameras available");
+          await cameraController.initialize().then((value) {
+            cameraController.startImageStream((image) {
+              cameraCount++;
+              if (cameraCount % 10 == 0) {
+                cameraCount = 0;
+                objectDetector(image);
+                // update();
+              }
+              update();
+            });
+          });
+          isCameraInitialised(true);
+          update();
+        } else {
+          print("No cameras available");
+        }
+      } catch (e) {
+        print("Error initializing camera: $e");
       }
-    } catch (e) {
-      print("Error initializing camera: $e");
+    } else {
+      print("Permsission Denied");
+    }
+  }
+
+  initTFlite() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/label.text",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+
+    if (detector != null) {
+      var ourDetectedObject = detector.first;
+      if (ourDetectedObject['confidenceInClass'] * 100 > 45) {
+        label = detector.first['detectedClass'].toString();
+        h = ourDetectedObject['rect']['h'];
+        w = ourDetectedObject['rect']['w'];
+        x = ourDetectedObject['rect']['x'];
+        y = ourDetectedObject['rect']['y'];
+      }
+      update();
+      // print('Result is $detector');
     }
   }
 }
--- lib/screen/camera_screen.dart
+++ lib/screen/camera_screen.dart
@@ -13,7 +13,32 @@ class CameraScreen extends StatelessWidget {
           init: ScanController(),
           builder: (controller) {
             return controller.isCameraInitialised.value
-                ? CameraPreview(controller.cameraController)
+                ? Stack(children: [
+                    CameraPreview(controller.cameraController),
+                    Positioned(
+                      top: (controller.y) * 700,
+                      right: (controller.x) * 500,
+                      child: Container(
+                        width: controller.w * 100 * context.width / 100,
+                        height: controller.h * 100 * context.height / 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            color: Colors.green,
+                            width: 4.0,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                                color: Colors.white,
+                                child: Text(controller.label)),
+                          ],
+                        ),
+                      ),
+                    )
+                  ])
                 : const Center(child: Text("Loading Preview..."));
           }),
     );
